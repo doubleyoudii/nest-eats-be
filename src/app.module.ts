970a268bdb662d20dcd4f5d69e2cb7324e095b60
William Ducta <iamwillducta@gmail.com>
2025-02-04 21:30:56 +0800

    feat: setup db pg with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './database/config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const infrastructureDatabaseModule = TypeOrmModule.forRootAsync({
+  useClass: TypeOrmConfigService, // database config
+  dataSourceFactory: async (option: DataSourceOptions) => {
+    return new DataSource(option).initialize();
+  },
+});
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: ['.env'],
+    }),
+    infrastructureDatabaseModule, // database setup
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
